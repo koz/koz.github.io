7c074361d17120acca8c475e9613adf5f333d18a
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-01-16 09:57:14 -0300

    Remove image from mobile

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const WCLink = document.getElementById('WCLink')
 const WCImage = document.getElementById('WCImage')
 
 WCLink.addEventListener('mouseenter', () => {
-  WCImage.classList.replace('dn', 'db')
+  WCImage.classList.replace('dn-ns', 'db-ns')
 })
 
 WCLink.addEventListener('mouseleave', () => {
-  WCImage.classList.replace('db', 'dn')
+  WCImage.classList.replace('db-ns', 'dn-ns')
 })
