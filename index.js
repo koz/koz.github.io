4f6af9845266730608f24aaefbbd23ed5b8f4bfc
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-01-16 22:10:41 -0300

    Make image position random

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const WCLink = document.getElementById('WCLink')
 const WCImage = document.getElementById('WCImage')
 
+const getRandomInt = (max, min = 0) => {
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+}
+
 WCLink.addEventListener('mouseenter', () => {
+  WCImage.style.right = `${getRandomInt(40, 20)}%`
+  WCImage.style.top = `${getRandomInt(50, 10)}%`
   WCImage.style.transform = 'scale(1)'
   WCImage.style.opacity = 1
 })
